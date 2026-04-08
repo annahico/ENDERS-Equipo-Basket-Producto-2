ce5256cee5d22d1b03f7de5a36d0dc0f01dcecb0
annahico <annahico@gmail.com>
2026-04-08 12:31:26 +0200

    feat: improved code for firbae

--- src/app/components/players-component/players-component.ts
+++ src/app/components/players-component/players-component.ts
@@ -1,40 +1,45 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { players } from '../../data//players';
+import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { Player } from '../../models/player';
 import { FormsModule } from '@angular/forms';
+import { PlayersService } from '../../services/players.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-players-component',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './players-component.html',
   styleUrl: './players-component.css',
 })
 export class PlayersComponent {
-  players = players;
-  showFilters = false;
-  searchText:string = '';
-  positionFilter:string = '';
-  ageFilter?: number;
-  filteredPlayers:Player[] = [];
-  @Output()
+  private playersService = inject(PlayersService);
 
+  players: Player[] = [];
+  showFilters = false;
+  searchText: string = '';
+  positionFilter: string = '';
+  ageFilter?: number;
+  filteredPlayers: Player[] = [];
+
+  @Output()
   playerSelected = new EventEmitter<Player>();
 
-  selectPlayer(player: Player){
-    console.log("Jugador Seleccionado: ", player);
-    this.playerSelected.emit(player);
-  }
-  ngOnInit(){
-    this.filteredPlayers = this.players;
+  ngOnInit() {
+    this.playersService.getPlayers().subscribe(data => {
+      this.players = data;
+      this.filteredPlayers = data;
+    });
   }
 
-  applyFilters(){
+  selectPlayer(player: Player) {
+    this.playerSelected.emit(player);
+  }
+
+  applyFilters() {
     this.filteredPlayers = this.players.filter(player => {
       const matchName = player.nombre.toLowerCase().includes(this.searchText.toLowerCase()) ||
                         player.apellidos.toLowerCase().includes(this.searchText.toLowerCase());
       const matchPosition = this.positionFilter === '' || player.posicion === this.positionFilter;
       const matchAge = !this.ageFilter || player.edad <= this.ageFilter;
-
       return matchName && matchPosition && matchAge;
     });
   }
