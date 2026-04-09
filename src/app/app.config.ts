332242d9ea70f7e4573b743e627081c2e19b3808
annahico <annahico@gmail.com>
2026-04-09 09:59:24 +0200

    feat: solved problems with dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,9 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from '../app/enviroments/enviroment';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
   ]
 };
