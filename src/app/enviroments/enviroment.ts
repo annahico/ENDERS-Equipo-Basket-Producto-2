72e3d77318b8e6f1a148ae75e892b16694ec5f04
annahico <annahico@gmail.com>
2026-04-12 18:45:56 +0200

    fix: added enviroment

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,1 +1,11 @@
-src/app/enviroments/enviroment.ts
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAAwpp9M4pmtJE7L27QRs5BTgrWWIqoNrw",
+    authDomain: "enders-58bd3.firebaseapp.com",
+    projectId: "enders-58bd3",
+    storageBucket: "enders-58bd3.firebasestorage.app",
+    messagingSenderId: "673433042167",
+    appId: "1:673433042167:web:f4ec257b750a19e01467f2"
+  }
+};
